ff0cc84a409ea086c994794b45ec40c37ed51529
Will <will.v.holcombe@gmail.com>
2024-06-07 11:07:32 -0500

    login pos and neg both working now

--- tests/e2e/e2e-login.spec.ts
+++ tests/e2e/e2e-login.spec.ts
@@ -1,15 +1,41 @@
 import { test, expect } from '@playwright/test'
 
+
+
 test.describe('Login / Logout Flow', () => {
   //Before Hook
 
   test.beforeEach(async ({ page }) => {
-    await page.goto("http://zero.webappsecurity.com/")
+    await page.goto("http://zero.webappsecurity.com/index.html")
   })
 
-  //Negative scenario
- 
+  //Negative Scenario
+
   test('Negative Scenario for login', async ({ page }) => {
+        await page.click('#signin_button')
+        await page.type('#user_login', 'invalid username')
+        await page.type('#user_password', 'invalid password')
+        await page.click('text=Sign in')
+    
+        const errorMessage = await page.locator('.alert-error')
+        await expect(errorMessage).toContainText('Login and/or password are wrong')
+      })
+    
+ 
+  // test('Negative Scenario for login', async ({ page }) => {
+  //   await page.click('#signin_button')
+  //   await page.type('#user_login', 'username')
+  //   await page.type('#user_password', 'password')
+  //   await page.click('text=Sign in')
+
+  //   const errorMessage = await page.locator('.alert-error')
+  //   await expect(errorMessage).toContainText('Login and/or password are wrong.')
+   
+  // })
+
+  //Positive Scenario
+
+  test('Positive Scenario for login', async ({ page }) => {
     await page.click('#signin_button')
     await page.type('#user_login', 'invalid username')
     await page.type('#user_password', 'invalid password')
