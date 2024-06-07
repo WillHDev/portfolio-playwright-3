bcad1f4a30cb38f7a010d6c97a3dd1210c0a7915
Will <will.v.holcombe@gmail.com>
2024-06-07 15:49:36 -0500

    first commit

--- tests/e2e/e2e-login.spec.ts
+++ tests/e2e/e2e-login.spec.ts
@@ -21,17 +21,7 @@ test.describe('Login / Logout Flow', () => {
         await expect(errorMessage).toContainText('Login and/or password are wrong')
       })
     
- 
-  // test('Negative Scenario for login', async ({ page }) => {
-  //   await page.click('#signin_button')
-  //   await page.type('#user_login', 'username')
-  //   await page.type('#user_password', 'password')
-  //   await page.click('text=Sign in')
 
-  //   const errorMessage = await page.locator('.alert-error')
-  //   await expect(errorMessage).toContainText('Login and/or password are wrong.')
-   
-  // })
 
   //Positive Scenario
 
